6dc95ea13ec9585a96f8d35facce02eafe63202f
celikozkan <ozkan44celik@gmail.com>
2020-10-16 19:13:50 +0300

    pwa

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ gem "ruby"
 gem "jekyll"
 
 group :jekyll_plugins do
+  gem "jekyll-pwa-plugin"
   gem "jekyll-sitemap"
   gem "jekyll-paginate"
   gem "jemoji"
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,34 @@
-self.addEventListener('install', function () {
-    console.log('Install!');
+// service-worker.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+    prefix: 'my-blog',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'runtime-cache'
 });
-self.addEventListener("activate", event => {
-    console.log('Activate!');
-});
-self.addEventListener('fetch', function (event) {
-    console.log('Fetch!', event.request);
-});
+
+// let Service Worker take control of pages ASAP
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// use `networkFirst` strategy for `*.html`, like all my posts
+workbox.routing.registerRoute(
+    /\.html$/,
+    workbox.strategies.networkFirst()
+);
+
+// use `cacheFirst` strategy for images
+workbox.routing.registerRoute(
+    /assets\/(img|icons)/,
+    workbox.strategies.cacheFirst()
+);
+
+// third party files
+workbox.routing.registerRoute(
+    /^https?:\/\/cdn.staticfile.org/,
+    workbox.strategies.staleWhileRevalidate()
+);
